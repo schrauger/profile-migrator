75d7d9de6c1ce0cf39a9ac87c3f405ed48504ad2
Stephen Schrauger <stephen@schrauger.com>
2020-03-06 15:57:18 -0500

    now in plugin format fixes all issues with categorization, images, repeaters no export/import needed. uses native WP functions to rename fields, and a simple sql query to modify post type

--- convert.py
+++ convert.py
@@ -9,33 +9,73 @@ parser = etree.XMLParser(strip_cdata=False)  # WP has CDATA xml tags
 
 def parse_xml(xml_file):
     tree = etree.parse(xml_file, parser=parser)
+    print("Dump parsed. Beginning changes.")
     root = tree.getroot()
     namespaces: Dict[str, str] = {
-        'wp': 'http://wordpress.org/export/1.2/'
+        'wp': 'http://wordpress.org/export/1.2/',
+        'content': "http://purl.org/rss/1.0/modules/content/"
     }
-    
-    for item in root.findall('./channel/item'):
-        single_profile = {}
-        
+    all_items = root.findall('./channel/item')
+    total = len(all_items)
+    for idx,item in enumerate(all_items):
+        print('Post ' + str(idx+1) + '/' + str(total))
+        #if idx > 5:
+        #    return etree.tostring(root, pretty_print=True)
         # change posttype from profiles to person
-        item = change_cdata(item.find('wp:post_type', namespaces), 'profiles', 'person')
+        change_cdata(item.find('wp:post_type', namespaces), 'person', 'profiles')
         
         # append order_by_name field
         step_concat_name(item, namespaces)
+
+        # change bio to be main content
+        step_biography_to_main_content(item, namespaces)
         
-        for child in item.findall('wp:postmeta/wp:meta_key', namespaces):
-            # job title
-            child = change_cdata(child, 'position', 'person_jobtitle')
-            child = change_cdata(child, '_position', '_person_jobtitle')
-            child = change_cdata(child, 'field_156', 'field_5953aa3d25c14')
-    print(etree.tostring(root))
+        # job title
+        postmeta_change(item, namespaces, 'position', 'person_jobtitle', 'field_156', 'field_5953aa3d25c14')
+        
+        # phone
+        postmeta_change(item, namespaces, 'phone', 'person_phone_numbers_0_number', 'field_158', 'field_5953aaca25c16')
+        
+        # fax
+        postmeta_change(item, namespaces, 'fax', 'person_phone_numbers_1_number', 'field_159', 'field_5953aaca25c16')
+        
+        # email
+        postmeta_change(item, namespaces, 'email', 'person_email', 'field_160', 'field_5953ac93b95b5')
+        
+        # office location
+        postmeta_change(item, namespaces, 'office_address', 'person_room', 'field_157', 'field_5953acb0b95b6')
+
+        # education/specialties
+        postmeta_change(item, namespaces, 'education', 'person_educationspecialties', 'field_162', 'field_5953acb0b95b6')
+        
+    return etree.tostring(root, pretty_print=True)
+    
+    
+    #print(etree.tostring(root, pretty_print=True))
+
+# changes a postmeta label and key from old profile system to match up with new profile system
+def postmeta_change(item_element, namespaces, old_label, new_label, old_ACF_key, new_ACF_key):
+    for child in item_element.findall('wp:postmeta/wp:meta_key', namespaces):
+        
+        change_cdata(child, new_label, old_label)
+        changed_underscore_key = change_cdata(child, '_' + new_label, '_' + old_label)
+        if changed_underscore_key:
+            child.xpath('../wp:meta_value', namespaces=namespaces)[0].text = etree.CDATA(new_ACF_key)
 
 
+# concatonates separate name fields into one order_by field
+# removes first_name, middle_initial, last_name
 def step_concat_name(item, namespaces):
     # append order_by_name field
     fname = get_cdata_val(item, 'first_name', namespaces)
+    if not fname:
+        fname = get_cdata_val(item, 'avf_first_name_1', namespaces)
     mname = get_cdata_val(item, 'middle_initial', namespaces)
+    if not mname:
+        mname = get_cdata_val(item, 'avf_middle_initial_1', namespaces)
     lname = get_cdata_val(item, 'last_name', namespaces)
+    if not lname:
+        lname = get_cdata_val(item, 'avf_last_name_1', namespaces)
     
     fullname = ''
     sortname = ''
@@ -52,10 +92,23 @@ def step_concat_name(item, namespaces):
     item.append(cdata_element_to_append('person_orderby_name', sortname))
     item.append(cdata_element_to_append('_person_orderby_name', 'field_59669002f433d'))
 
+    #delete_postmeta(item, 'first_name', namespaces)
+    #delete_postmeta(item, 'middle_initial', namespaces)
+    #delete_postmeta(item, 'last_name', namespaces)
+    #delete_postmeta(item, 'avf_first_name_1', namespaces)
+    #delete_postmeta(item, 'avf_middle_initial_1', namespaces)
+    #delete_postmeta(item, 'avf_last_name_1', namespaces)
+
+# moves biography data to the main content field
+def step_biography_to_main_content(item, namespaces):
+    bio = get_cdata_val(item, 'biography', namespaces)
+    
+    change_cdata(item.find('content:encoded', namespaces), bio)
+
 
 # gets the cdata value from a sub-element of the specified element. the sub-element is matched against text provided
 def get_cdata_val(element, name, namespaces):
-    el = element.xpath('//wp:postmeta[wp:meta_key[text()="' + name + '"]/text()]/wp:meta_value/text()',
+    el = element.xpath('./wp:postmeta[wp:meta_key[text()="' + name + '"]]/wp:meta_value/text()',
                        namespaces=namespaces)
     if el:
         return el[0]
@@ -72,20 +125,43 @@ def cdata_element_to_append(name, value):
     el_meta_key.text = etree.CDATA(name)
     el_meta_value = etree.SubElement(el_postmeta, wp_prefix + "meta_value")
     el_meta_value.text = etree.CDATA(value)
+    
     return el_postmeta
 
 
 # returns an element with cdata text changed
-def change_cdata(element, oldtext, newtext):
-    print(element.text)
-    if element.text.lower() == oldtext:
-        element.text = newtext
-    print(element.text)
-    return element
+def change_cdata(element, newtext, oldtext = None):
+    # print(element.text)
+    if oldtext:
+        if element.text.lower() == oldtext: # only replace the element if oldtext matches; parent is looping
+            #print(element.text)
+            element.text = etree.CDATA(newtext)
+            return True
+    else: #oldtext isn't defined; blindly replace the element given
+        element.text = etree.CDATA(newtext)
+        return True
+    #print(element.text)
+    return False
 
+
+# changes the meta_value for a specified meta_key - used mainly for ACF backend field unique ids, stored in the value.
+#def change_cdata_val(element, newvalue, oldvalue):
+#    element.xpath('../wp:meta_value').text = etree.CDATA(newvalue)
+    
+
+# deletes a postmeta element which contains a child element matching the name provided
+# also deletes the matching postmeta field that has a prepended underscore
+def delete_postmeta(element, name, namespaces):
+    for to_delete in element.xpath('./wp:postmeta[wp:meta_key[text()="' + name + '"]]', namespaces=namespaces):
+        to_delete.getparent().remove(to_delete)
+
+    for to_delete in element.xpath('./wp:postmeta[wp:meta_key[text()="_' + name + '"]]', namespaces=namespaces):
+        to_delete.getparent().remove(to_delete)
 
 # def change_tag(element, newtag):
 
 
 with open('dump.xml') as xmldump:
-    parse_xml(xmldump)
+    output_xml_string = parse_xml(xmldump)
+with open('output.xml', 'wb') as output:
+    output.write(output_xml_string)
---
+++ profile-migrator.php
@@ -0,0 +1,226 @@
+<?php
+/*
+Plugin Name: Profile Migrator
+Plugin URI: https://github.com/schrauger/profile-migrator
+Description: One-shot plugin. Converts profiles from old UCF COM theme to the new Colleges-Theme style.
+Version: 1.1
+Author: Stephen Schrauger
+Author URI: https://github.com/schrauger/profile-migrator
+License: GPL2
+*/
+
+class profile_migrator {
+	static function run_network_migration(){
+		$all_sites = get_sites();
+
+		foreach ($all_sites as $site){
+			switch_to_blog($site->blog_id);
+				self::convert();
+			restore_current_blog();
+		}
+		//self::convert();
+
+	}
+
+	// Loops through all profile types
+	static function convert(){
+		self::alter_post_type();
+		self::alter_acf_references();
+	}
+
+	/**
+	 * Alters the database to change post types from 'profiles' to 'person' for all existing 'profiles' posts
+	 */
+	static function alter_post_type(){
+		global $wpdb;
+		$old_post_types = array('profiles' => 'person');
+		foreach ($old_post_types as $old_type=>$new_type) {
+			$wpdb->query( $wpdb->prepare( "UPDATE {$wpdb->posts} SET post_type = REPLACE(post_type, %s, %s) 
+                         WHERE post_type LIKE %s", $old_type, $new_type, $old_type ) );
+			$wpdb->query( $wpdb->prepare( "UPDATE {$wpdb->posts} SET guid = REPLACE(guid, %s, %s) 
+                         WHERE guid LIKE %s", "post_type={$old_type}", "post_type={$new_type}", "%post_type={$old_type}%" ) );
+			$wpdb->query( $wpdb->prepare( "UPDATE {$wpdb->posts} SET guid = REPLACE(guid, %s, %s) 
+                         WHERE guid LIKE %s", "/{$old_type}/", "/{$new_type}/", "%/{$old_type}/%" ) );
+		}
+	}
+
+	static function alter_acf_references(){
+		$loop = new WP_Query(
+			array(
+				'post_type' => ['person','profiles'],
+				'posts_per_page' => 1000,
+				'offset' => 2000,
+				//'s' => 'Deborah German'
+			)
+		);
+//		echo 'p: ' . $loop->post_count . '; ';
+		while ($loop->have_posts()) {
+			$loop->the_post();
+			self::alter_acf_reference('position','person_jobtitle');
+			self::alter_acf_sub_reference('phone','person_phone_numbers','number', 1);
+			self::alter_acf_sub_reference('fax','person_phone_numbers','number', 2);
+			self::alter_acf_reference('email','person_email');
+			self::alter_acf_reference('office_address','person_room');
+			self::alter_acf_reference('education','person_educationspecialties');
+			self::concatonate_old_acf_to_new_field(
+				[
+					[
+						'acf' => 'last_name',
+						'post' => ', '
+					],
+					[
+						'acf' => 'first_name',
+						'post' => ' '
+					],
+					'middle_initial'
+				 ], 'person_orderby_name'
+			);
+			self::concatonate_old_acf_to_new_field(
+				[
+					[
+						'acf' => 'avf_last_name_1',
+						'post' => ', '
+					],
+					[
+						'acf' => 'avf_first_name_1',
+						'post' => ' '
+					],
+					'avf_middle_initial_1'
+				 ], 'person_orderby_name'
+			);
+			self::biography_to_main();
+			self::image_to_featured();
+		}
+	}
+
+	/**
+	 * Takes an old acf field name, grabs the value, and stores it in a new field name.
+	 * Will not overwrite a new field if it already has data.
+	 * @param string $old_name
+	 * @param string $new_field
+	 */
+	static function alter_acf_reference(string $old_field, string $new_field){
+		if (class_exists('acf')) { // simple check to make sure acf is installed and running at this point
+
+			$new_value = get_field( $new_field );
+
+			if ( ! $new_value ) {
+				$old_value = get_field( $old_field );
+				if ($old_value) {
+					// if new field is empty, and old field has something, then copy old value to the new field
+					update_field( $new_field, $old_value );
+				}
+			} else {
+				// do nothing. don't overwrite if new field already has data (already migrated?),
+				// and don't bother setting an empty new field if there's no data in the old field.
+//echo 'HI.';
+			}
+		}
+	}
+
+	/**
+	 * @param string $old_field
+	 * @param string $new_field_parent
+	 * @param string $new_field
+	 * @param int    $index (1-based)
+	 */
+	static function alter_acf_sub_reference(string $old_field, string $new_field_parent, string $new_field, int $index){
+		if (class_exists('acf')) { // simple check to make sure acf is installed and running at this point
+
+
+			$repeater_values = get_field($new_field_parent);
+			$first_row = $repeater_values[$index - 1]; // acf indexes are 1 based, but php arrays are 0 based.
+			$new_value = $first_row[$new_field];
+
+			if ( ! $new_value ) {
+				$old_value = get_field( $old_field );
+				if ($old_value) {
+					// if new field is empty, and old field has something, then copy old value to the new field
+					update_sub_field( [ $new_field_parent, $index, $new_field ], $old_value );
+				}
+			} else {
+				// do nothing. don't overwrite if new field already has data (already migrated?),
+				// and don't bother setting an empty new field if there's no data in the old field.
+			}
+		}
+	}
+
+	/**
+	 * Takes an array of old acf field names, concatonates the values, and saves it to a single new field.
+	 * $old_fields is an array with either simple values (the acf field name),
+	 * or an inner array with optional 'pre' and 'post' string values, and a required 'acf' field name.
+	 * Will not overwrite a new field if it already has data.
+	 *
+	 * @param array $old_fields
+	 *              ['last','first','middle']
+	 *              or
+	 *              [
+	 *                  ['acf'=>'last', 'post'=>','],
+	 *                  ['acf'=>'first', 'pre'=> ' ', 'post'=>' '],
+	 *                  'middle'
+	 *              ]
+	 * @param       $new_field
+	 */
+	static function concatonate_old_acf_to_new_field(Array $old_fields, $new_field){
+		if (class_exists('acf')) { // simple check to make sure acf is installed and running at this point
+			$concat_value = '';
+			$new_value = get_field( $new_field );
+
+			foreach ($old_fields as $field) {
+				if (is_array($field)){
+					if ($field['acf']){
+						if ($field['pre']){
+							$concat_value .= $field['pre'];
+						}
+						$concat_value .= get_field($field['acf']);
+						if ($field['post']){
+							$concat_value .= $field['post'];
+						}
+					}
+				} else {
+					$concat_value .= get_field($field);
+				}
+			}
+			if ( ! $new_value && $concat_value ) {
+				// if new field is empty, and old field has something, then copy old value to the new field
+				update_field( $new_field, $concat_value );
+			} else {
+				// do nothing. don't overwrite if new field already has data (already migrated?),
+				// and don't bother setting an empty new field if there's no data in the old field.
+			}
+		}
+	}
+
+	// converts the 'biography' field to be the main post content
+	static function biography_to_main(){
+		global $post;
+		$old_value = get_field( 'biography' );
+
+		if (empty($post->post_content)){
+			 wp_update_post([
+			 	'ID' => $post->ID,
+			 	'post_content' => $old_value
+			 ]);
+
+		} else {
+			// do nothing. don't overwrite if new field already has data (already migrated?),
+			// and don't bother setting an empty new field if there's no data in the old field.
+		}
+	}
+
+	// converts the image in the acf field to the featured image
+	static function image_to_featured(){
+		global $post;
+
+		if (!(has_post_thumbnail())){
+			$old_value = get_field('photo');
+			if ($old_value) {
+				set_post_thumbnail( $post, $old_value );
+			}
+		}
+	}
+}
+
+// run profile migration upon plugin activation
+register_activation_hook(__FILE__, ['profile_migrator','run_network_migration']);
+add_action('init',['profile_migrator','run_network_migration']);
